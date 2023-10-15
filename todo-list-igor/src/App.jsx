5fec1a4a7d7967622a461457b121c9e76a0309fe
IgorSPinto <igorpinto103@gmail.com>
2023-10-14 21:43:28 -0300

    feat: :sparkles: Estrturação do layout base da página

--- todo-list-igor/src/App.jsx
+++ todo-list-igor/src/App.jsx
@@ -5,29 +5,48 @@ function App() {
 
     {
       id:1,
-      text: "criar funcionalidade x no sistema",
-      category: "Trabalho",
-      isCompleted: false,
+      texto: "criar funcionalidade x no sistema",
+      categoria: "Trabalho",
+      Completo: false,
     },
     {
       id:2,
-      text: "Ir pra academia",
-      category: "Pessoal",
-      isCompleted: false,
+      texto: "Ir pra academia",
+      categoria: "Pessoal",
+      Completo: false,
     },
     {
       id:3,
-      text: "Estudar React",
-      category: "Estudos",
-      isCompleted: false,
+      texto: "Estudar React",
+      categoria: "Estudos",
+      Completo: false,
     }
   ])
 
-  return (
-   <div>
-      Salve salve familia!
-   </div>
-  )
+  return <div className='app'>
+    <h1>Lista de Tarefas</h1>
+    <div className='todo-list'>
+
+      {/* map de array que exibe todos os objetos da todo list */}
+      {todos.map((todo) => (
+        <div className='todo'>
+          <div className="conteudo">
+            <p>{todo.texto}</p>
+            <p className="categoria">
+              ({todo.categoria})
+            </p>
+          </div>
+          <div>
+            {/* botão que irá marcar uma tarefa como concluída */}
+            <button>Completar Tarefa</button>
+            
+            {/* botão que irá deletar uma tarefa */}
+            <button>Deletar Tarefa</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
 }
 
 export default App
